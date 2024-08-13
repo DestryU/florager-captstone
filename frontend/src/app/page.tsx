1005a986ee6f167f7e8c76f29dd9df041bfeee13
Destry Ullrich <wullrich@cnm.edu>
2024-08-13 15:57:45 -0600

    setup components file and directory for map components

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,19 @@
+import hotbar
+
 export default function Home() {
     return (
-        <h1 className={"text-3xl font-bold underline"}>Is this thing on?</h1>
+        <>
+
+            <h1 className={"text-3xl font-bold underline"}>Is this thing on?</h1>
+
+
+
+
+
+
+
+
+
+        </>
     )
 }
