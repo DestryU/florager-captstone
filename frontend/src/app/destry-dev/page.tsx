9007943b254028653f5c866f7bae2f595412348d
Destry Ullrich <wullrich@cnm.edu>
2024-08-16 11:55:34 -0600

    cleaned and reorganzied page structures; removed all development code from app/page; created destry-dev directory; created custom page in destry-dev for personal testing.

---
+++ frontend/src/app/destry-dev/page.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+import React from 'react'
+import Overlay from "@/app/components/Overlay";
+
+export default function Home() {
+    return (
+        <>
+            <Overlay />
+
+            <h1 className={"text-3xl font-bold underline"}>Is this thing on?</h1>
+
+
+        </>
+    )
+}
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,16 +1,5 @@
-'use client'
-
-import React from 'react'
-import Overlay from "@/app/components/Overlay";
-
 export default function Home() {
     return (
-        <>
-            <Overlay />
-
-            <h1 className={"text-3xl font-bold underline"}>Is this thing on?</h1>
-
-
-        </>
+        <h1 className={"text-3xl font-bold underline"}>Is this thing on?</h1>
     )
 }
