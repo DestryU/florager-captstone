321fb63e3cddbf8e5d7bf4ed72717b6893601a10
Melissa E. Chavez <MChavez905@cnm.edu>
2024-08-22 21:13:34 -0600

    Adding sylistic touches to Carousel

--- frontend/src/app/components/MainCarousel.tsx
+++ frontend/src/app/components/MainCarousel.tsx
@@ -8,10 +8,10 @@ export function MainCarousel() {
     return (
         <div className="main-carousel">
             <Carousel pauseOnHover>
-                <Image src={"/images/NMpic2.jpg"} alt={"NM pic"} height={600} width={600}/>
-                <Image src={"/images/NMpic7.jpg"} alt={"NM pic"} height={600} width={600}/>
-                <Image src={"/images/NMpic10.jpg"} alt={"NM pic"} height={600} width={600}/>
-                <Image src={"/images/NMpic9.jpg"} alt={"NM pic"} height={600} width={600}/>
+                <Image src="/images/NMpic2.jpg" duration-15000 alt="NM pic" height={1000} width={600}/>
+                <Image src="/images/NMpic7.jpg" duration-15000 alt="NM pic" height={600} width={600}/>
+                <Image src="/images/NMpic10.jpg" duration-15000 alt="NM pic" height={600} width={600}/>
+                <Image src="/images/NMpic9.jpg" duration-15000 alt="NM pic" height={600} width={600}/>
             </Carousel>
         </div>
     );
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,17 +5,20 @@ export default function Home() {
     return (
         <>
             <MainCarousel/>
-            <div className={" md:flex-row items-center px-[1rem] pb-[2rem] pt-[1rem] bg-neutral-700"}>
-                <div className={"flex flex-col md:flex-row items-center justify-center rounded-2xl bg-dk-green-25 border-2"}>
-                <button className={"flex flex-col justify-around identify-background bg-no-repeat bg-cover bg-center size-44 rounded-full text-center m-[3rem] drop-shadow-md border-8 border-gray-200-950 opacity-65"}>
-                    <p className={"text-white text-lg md:text-center"}>Identify NM Plants</p>
+            <div className={" md:flex-row items-center px-[1.5rem] pb-[2rem] pt-[2rem] bg-green-900"}>
+                <div className={"flex flex-col md:flex-row items-center justify-center rounded-2xl bg-white border-5"}>
+                <button className={"flex flex-col justify-around identify-background bg-no-repeat bg-cover bg-center size-44 rounded-full text-center m-[3rem] border-solid border-b-cyan-400 border-4"}>
+                    <p className={"text-white text-2xl md:text-center"}>Identify NM Plants</p>
                 </button>
-                <button className={"flex flex-col justify-around identify-background bg-no-repeat bg-cover bg-center h-44 w-44 rounded-full text-center m-[3rem]"}>
-                    <p className={"text-white text-lg inline-block align-middle"}>Explore NM Plants</p>
+                <button className={"flex flex-col justify-around identify-background bg-no-repeat bg-cover bg-center h-44 w-44 rounded-full text-center m-[3rem] border-solid border-b-cyan-400 border-4"}>
+                    <p className={"text-white text-2xl inline-block align-middle"}>Explore NM Plants</p>
                 </button>
-                <button className={"flex flex-col justify-around identify-background bg-no-repeat bg-cover bg-center h-44 w-44 rounded-full text-center m-[3rem]"}>
-                    <p className={"text-white text-lg inline-block align-middle"}>Share NM Plants</p>
+                <button className={"flex flex-col justify-around identify-background bg-no-repeat bg-cover bg-center h-44 w-44 rounded-full text-center m-[3rem] border-solid border-b-cyan-400 border-4"}>
+                    <p className={"text-white lg:text-2xl inline-block align-middle"}>Share NM Plants</p>
                 </button>
+                </div>
+                <div>
+
                 </div>
             </div>
         </>
