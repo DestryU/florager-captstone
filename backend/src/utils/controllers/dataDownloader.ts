f6029010d13433cbfb0d01e0bb2343613ee7dca7
Melissa E. Chavez <MChavez905@cnm.edu>
2024-09-07 15:33:18 -0600

    debugging dataDownloader for the .result[].media

--- backend/src/utils/controllers/dataDownloader.ts
+++ backend/src/utils/controllers/dataDownloader.ts
@@ -13,24 +13,23 @@ async function dataDownloader() {
    }  */
     let data = await fetch(`https://my-api.plantnet.org/v2/projects/k-southwestern-u-s-a/species?lang=en&api-key=${process.env.PLANTNET_API_KEY}`)
      .then(res => res.json())
-   for(let i=0; i<data.length; i++){
+   for(let i=0; i<10; i++){
        let plant = {
           plant_id: null,
           plant_scientific_name: '' ,
           plant_common_names: [],
           plant_image_url: 'https://ourURL.com',
-          plant_reference_url: `https://www.gbif.org/species/${data[0].gbifId}`
+          plant_reference_url: `https://www.gbif.org/species/${data[i].gbifId}`
        }
-       break
-       let individualdata = await fetch(`https://www.gbif.org/api/occurrence/search?limit=200&taxonKey=${process.env.TAXON_API_KEY}`)
-    .then(res => res.json())
-    for(let j= 0; j < individualdata.length; j++){
-    let plantImageUrl = individualdata.filter
-    return plantImageUrl
-      }
-   }
+       let individualdata = await fetch(createUrl(data[i].gbifId))
+           .then(res => res.json())
+       console.log(individualdata.result[i].media)
 
-    console.log(data[0])
- }
+    }
+
+  }
+function createUrl(gbifId: string) {
+ return `https://www.gbif.org/api/occurrence/search?limit=200&taxonKey=${gbifId}&typeStatus=TYPE&typeStatus=TYPE_SPECIES&typeStatus=TYPE_GENUS&typeStatus=ALLOLECTOTYPE&typeStatus=ALLONEOTYPE&typeStatus=ALLOTYPE&typeStatus=COTYPE&typeStatus=EPITYPE&typeStatus=EXEPITYPE&typeStatus=EXHOLOTYPE&typeStatus=EXISOTYPE&typeStatus=EXLECTOTYPE&typeStatus=EXNEOTYPE&typeStatus=EXPARATYPE&typeStatus=EXSYNTYPE&typeStatus=EXTYPE&typeStatus=HAPANTOTYPE&typeStatus=HOLOTYPE&typeStatus=ICONOTYPE&typeStatus=ISOLECTOTYPE&typeStatus=ISOPARATYPE&typeStatus=ISONEOTYPE&typeStatus=ISOSYNTYPE&typeStatus=ISOTYPE&typeStatus=LECTOTYPE&typeStatus=NEOTYPE&typeStatus=PARALECTOTYPE&typeStatus=PARANEOTYPE&typeStatus=PARATYPE&typeStatus=PLASTOHOLOTYPE&typeStatus=PLASTOISOTYPE&typeStatus=PLASTOLECTOTYPE&typeStatus=PLASTONEOTYPE&typeStatus=PLASTOPARATYPE&typeStatus=PLASTOSYNTYPE&typeStatus=PLASTOTYPE&typeStatus=SECONDARYTYPE&typeStatus=SUPPLEMENTARYTYPE&typeStatus=SYNTYPE&typeStatus=TOPOTYPE&typeStatus=ORIGINALMATERIAL`
+}
 
 dataDownloader().catch(error => console.error(error))
