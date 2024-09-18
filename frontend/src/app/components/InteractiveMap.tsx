a9170beeafc1667beea3a816a458b7a647f4ba00
r-wagoner <rwagoner@cnm.edu>
2024-09-17 18:17:58 -0600

    still can't push changes.Waiting for help.

--- frontend/src/app/components/InteractiveMap.tsx
+++ frontend/src/app/components/InteractiveMap.tsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import {Map} from "@/app/components/Map";
-import {fetchFindRecent} from "@/utils/actions/find/find.action";
-
-
-export default async function Home() {
-
-    const finds = await fetchFindRecent()
-    console.log(finds)
-
-    return (
-        <>
-
-            <Map finds={finds}/>
-        </>
-    )
-}
+// import React from 'react'
+// import {Map} from "@/app/components/Map";
+// import {fetchFindRecent} from "@/utils/actions/find/find.action";
+//
+//
+// export default async function Home() {
+//
+//     const finds = await fetchFindRecent()
+//     console.log(finds)
+//
+// //     return (
+// //         <>
+// //
+// //             <Map finds={finds}/>
+// //         </>
+// //     )
+// // }
