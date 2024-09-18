115779f9866eb13fcd872a45e151232f1ee9af20
Shaya Torres <storres133@cnm.edu>
2024-09-18 14:19:32 -0600

    updated find card

--- frontend/src/app/components/FindCard.tsx
+++ frontend/src/app/components/FindCard.tsx
@@ -4,8 +4,9 @@ import React from "react";
 import { Card } from "flowbite-react";
 
 import {fetchFindId, fetchFindRecent} from "@/utils/actions/find/find.action";
-import {fetchPlantById} from "@/utils/actions/plant/plant.action";
+
 import {getProfileByProfileId} from "@/utils/actions/profile/profile.action";
+import {fetchPlantById} from "@/utils/actions/plant/plant.model";
 
 
 type Props = {
